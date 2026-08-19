92c5800608d99c8ebd5b5fe71ec4bf749f34335f
JackChuaYC <jackchuayc@gmail.com>
2026-08-20 00:55:18 +0800

    Add tasklist function

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Yawned User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Yawned.java
+++ src/main/java/Yawned.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Yawned {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] listOfTasks = new String[100];
+        int taskCounter = 0;
         String banner = "========================\n"
                 + "         YAWNED\n"
                 + "   Your sleepy chatbot\n"
@@ -15,25 +17,46 @@ public class Yawned {
         String userInput = getUserInput(scanner, "*Yawns..* You woke me up...\nWhat do you want?\n");
         printBreakLine();
         while (!userInput.equals("bye")) {
-            userInput = getUserInput(scanner, userInput);
+            if (userInput.equals("list")) {
+                printTaskList(listOfTasks, taskCounter);
+                userInput = getUserInput(scanner, "");
+            } else {
+                taskCounter = addTask(listOfTasks, userInput, taskCounter);
+                userInput = getUserInput(scanner, String.format("added: %s", userInput));
+            }
             printBreakLine();
         }
-        System.out.println("");
         printBreakLine();
     }
 
-    /*
+    /**
      *Prints the breakline for clearer "new command"
      */
-    public static void printBreakLine() {
+    private static void printBreakLine() {
         System.out.println("____________________________________________________________\n");
     }
 
-    /*
+    /**
+     * Gets input from user.
+     *
+     * @param Scanner scanner object to get input.
+     * @param String message output message to user.
+     * @return string object.
      */
-    public static String getUserInput(Scanner scanner, String message) {
+    private static String getUserInput(Scanner scanner, String message) {
         System.out.println(message);
         printBreakLine();
         return scanner.nextLine();
+    }
+
+    private static int addTask(String[] listOfTasks, String task,int taskCounter) {
+        listOfTasks[taskCounter] = task;
+        return ++taskCounter;
+    }
+
+    private static void printTaskList(String[] listOfTasks, int taskCounter) {
+        for (int i = 0; i < taskCounter; i++) {
+            System.out.printf("%d. %s%n", i+1, listOfTasks[i]);
+        }
     }
 }
