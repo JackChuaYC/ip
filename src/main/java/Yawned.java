c69dd96a5cd606a4bb11ba4c523e2a4c422b9168
JackChuaYC <jackchuayc@gmail.com>
2026-08-19 21:40:33 +0800

    Edited chatbot name. Added greeting and exit

---
+++ src/main/java/Yawned.java
@@ -0,0 +1,19 @@
+/**
+ * Entry point for the Yawned chatbot application.
+ */
+public class Yawned {
+    public static void main(String[] args) {
+        String breakline = "____________________________________________________________";
+        String banner = "========================\n"
+                + "         YAWNED\n"
+                + "   Your sleepy chatbot\n"
+                + "========================\n";
+        System.out.println(breakline);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Yawned\nWhat can I do for you?");
+        System.out.println(breakline);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(breakline);
+    }
+
+}
